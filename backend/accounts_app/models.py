13f508f8651f2c26a10175fe72e688e7ff282b14
pharaoh <pharaohmanson@gmail.com>
2021-08-08 17:53:39 -0400

    setting up the models for user accounts when logging in

--- backend/accounts_app/models.py
+++ backend/accounts_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from phone_field import PhoneField
 
-# Create your models here.
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError(
+                "Debe proporcionar un correo electrónico para crear una cuenta")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name,
+                          last_name=last_name)
+
+        user.set_password(password)
+        user.save()
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = phone_number = PhoneField(
+        blank=True, help_text='Contact phone number')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def get_full_name(self):
+        return self.first_name + last_name
+
+    def __str__(self):
+        return self.email
