1ef877f76273fa9c1fc82b8bed1711b09cfba15c
pharaoh <pharaohmanson@gmail.com>
2021-08-09 12:59:37 -0400

    working on login and redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,12 @@ const App = () => (
         <Layout>
             <Switch>
                 <Route exact path='/' component={Home}/>
+                <Route exact path='/login' component={Login}/>
+                <Route exact path='/signup' component={Signup}/>
+                <Route exact path='/rest_password' component={ResetPassword}/>
+                <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm}/>
+                <Route exact path='/activate/:uid/:token' component={Activate}/>
+                
             </Switch>
         </Layout>
     </Router>
--- frontend/src/containers/Login.jsx
+++ frontend/src/containers/Login.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Link, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux'
 
+const Login = () => {
+    
+    const [formData, setFormData] = useState({
+        email:"",
+        password:""
+    });
 
-const Login = () => (
-    <div>
-        Login
-    </div>
-)
+    const {email, password} = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
+    
+    const onSubmit = e =>{
+        e.preventDefault()
+        //login(email, password)
+    }
+
+    //Is the user authenticated
+    //Redirect them to the home page
+
+    return (
+        <>
+            <div className='container mt-5'>
+                <h1>Sign In</h1>
+                <p>Sign into your account</p>
+                <form onSubmit={e=> onSubmit(e)}>
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            type='email'
+                            placeholder='email'
+                            name='email'
+                            value={email}
+                            onChange={e => onChange(e)}
+                            required
+                            
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            type='password'
+                            placeholder='password'
+                            name='password'
+                            value={password}
+                            onChange={e => onChange(e)}
+                            minLength='6'
+                            required
+                        />
+                    </div>
+                    <button className='btn btn-primary' type='submit'>Login</button>
+                </form>
+                <p>Don't have an account? <Link to='/signup'>Sign up</Link></p>
+                <p>Forgot your password <Link to='/rest-password'>Reset Password</Link></p>
+            </div>
+        </>
+    )
+};
+
+const mapStateToProps = state => ({
+    //isauthenticated
+})
 
 export default Login;
 
