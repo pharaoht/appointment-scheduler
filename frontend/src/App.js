23ecda08453cff51918d4ad360ba95c8b3d65b88
pharaoh <pharaohmanson@gmail.com>
2021-08-09 10:32:57 -0400

    added boiler plate code to files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+import Home from './containers/Home'
+import Login from './containers/Login'
+import Signup from './containers/Signup'
+import Activate from './containers/Activate'
+import ResetPassword from './containers/ResetPassword'
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm'
 const App = () => {
     <Router>
         <Switch>
